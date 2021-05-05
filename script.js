173d497650f77f60ec55b5ca359335a6e5f33c6b
Andrew Masih <armasih@outlook.com>
2021-05-05 00:14:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function multiply (a,b) {
+  console.log ("a is:" + a);
+  console.log ("b is:" + b);
+/*a functon parameters are the special variables used by a function to handle this input, where as the arguments are the values provided for the parameers when we run the function  */
+
+
+}
+
+/*these two numbers are the arguments that are past to the code block: 'return a *b:' such that 'a' is replaced with 10, while 'b' is replaced by 2, if you switch the position: 'multiply (2, 10)'*/
+multiply (10, 2);
+/*multiply (2, 10); */
+
